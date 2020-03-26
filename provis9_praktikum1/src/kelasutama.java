a31311fd0d12b8ac2e593ee40a0e7bd611608392
Aris J <Aris J@julianlase>
2020-03-26 17:30:41 +0700

    ini update terbaru

---
+++ provis9_praktikum1/src/kelasutama.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aris J
+ */
+public class kelasutama {
+public static void main(String[] args){
+     //System.out.println("hello world");
+     //menghidupkan objek atau instansiasi
+     //futama.tampil("program pertama GUI");
+     
+     futama utama = new futama();
+     //utama.setTitle("pemrograman provis");
+     utama.setVisible(true);
+ }   
+}
